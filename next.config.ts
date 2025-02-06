dfb2701bd803d8d6f9654707acd1bbaa69dd89da
Isabelle Oktay <iso212@nyu.edu>
2025-02-06 14:21:50 +0100

    modify configs for production

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,16 @@
 import type { NextConfig } from 'next';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-/* module.exports = {
-  output: 'export',
-  basePath: '/sibelle-website',
-  assetPrefix: '/sibelle-website',
-}; */
+module.exports = {
+  output: 'export', // Tells Next.js to generate static files
+  distDir: 'out',
+  basePath: '/si-belle-website', // Change this to match your repo name
+  assetPrefix: isProd ? '/si-belle-website/' : '',
+};
 
 export default nextConfig;
